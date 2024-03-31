a88a49dccf62197a2ac813d09a7699c5fb19613f
Chris <{ID}+{username}@users.noreply.github.com>
2024-03-31 01:11:02 -0400

    Initial commit

---
+++ flake.nix
@@ -0,0 +1,69 @@
+{
+  description = "Chris Heath's NixOS Flake";
+
+  inputs = {
+    nixpkgs.url = "nixpkgs/nixos-23.11";
+    home-manager.url = "github:nix-community/home-manager/release-23.11";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+
+    # use the following for unstable:
+    # nixpkgs.url = "nixpkgs/nixos-unstable";
+
+    # or any branch you want:
+    # nixpkgs.url = "nixpkgs/{BRANCH-NAME}";
+  };
+
+  outputs = { self, nixpkgs, home-manager, ... }:
+    let
+      system = "x86_64-linux";
+      lib = nixpkgs.lib;
+      pkgs = nixpkgs.legacyPackages.${system};
+
+    
+      # ---- SYSTEM SETTINGS ---- #
+      systemSettings = {
+        system = "x86_64-linux"; # system arch
+        hostname = "chris-nix"; # hostname
+        #profile = "personal"; # select a profile defined from my profiles directory
+        timezone = "America/New_York"; # select timezone
+        locale = "en_US.UTF-8"; # select locale
+        bootMode = "uefi"; # uefi or bios
+        bootMountPath = "/boot"; # mount path for efi boot partition; only used for uefi boot mode
+        grubDevice = ""; # device identifier for grub; only used for legacy (bios) boot mode
+      };
+
+      # ----- USER SETTINGS ----- #
+      userSettings = {
+        username = "chris"; # username
+        name = "Chris"; # name/identifier
+        email = "heath9311@gmail.com"; # email (used for certain configurations)
+        dotfilesDir = "~/.dotfiles"; # 
+      };
+
+
+    in {
+      nixosConfigurations = {
+        ${systemSettings.hostname} = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+          specialArgs = {
+            # pass config variables from above
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        };
+      };
+      homeConfigurations = {
+        ${userSettings.username} = home-manager.lib.homeManagerConfiguration {
+          inherit pkgs;
+          modules = [ ./home.nix ];
+          extraSpecialArgs = {
+            # pass config variables from above
+            inherit systemSettings;
+            inherit userSettings;
+          };
+        };
+      };
+    };
+
+}
